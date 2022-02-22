d35ac42899f2990f14aa3070b17733cd5b197516
ThierrydelCastillo <dev@del-castillo.fr>
2022-02-22 15:01:43 +0100

    [PSF 03] Sécurité de nos pages PHP

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
-<?php include('header.php') ?>
+<?php include('../header.php') ?>
 
 <h1>Page d'accueil</h1>
 <p>
     <a href="/contact.php">Page de contact</a>
 </p>
 
-<?php include('footer.php') ?>
+<?php include('../footer.php') ?>
