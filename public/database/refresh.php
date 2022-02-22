e1748db4da44d22ce56eec7f16c4cd41a7b6af89
ThierrydelCastillo <dev@del-castillo.fr>
2022-02-22 15:40:18 +0100

    [PSF 05] Création de la base de données et migrations

---
+++ public/database/refresh.php
@@ -0,0 +1,17 @@
+<?php
+
+$path = __DIR__ . '/database.sqlite' ;
+if(file_exists($path)) unlink($path); // Efface le fichier database
+
+$pdo = new PDO("sqlite:$path");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query = $pdo->prepare('
+        CREATE TABLE posts(
+            id INTEGER PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            body TEXT NOT NULL
+        )
+    ');
+
+$query->execute();
