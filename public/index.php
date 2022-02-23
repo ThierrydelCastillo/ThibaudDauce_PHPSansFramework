b1924718d076b8f732c8118fb2b6c233ad1e24e2
ThierrydelCastillo <dev@del-castillo.fr>
2022-02-23 18:00:12 +0100

    [PSF 07] Affichage de la liste des articles

--- public/index.php
+++ public/index.php
@@ -1,6 +1,23 @@
 <?php include('../header.php') ?>
 
+<?php
+    $path = __DIR__ . '/../database/database.sqlite';
+    $pdo = new PDO("sqlite:$path");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $query = $pdo->prepare('SELECT * FROM posts');
+    $result = $query->execute();
+    $posts = $query->fetchAll();
+?>
+
 <h1>Page d'accueil</h1>
+
+<ul>
+    <?php foreach($posts as $post): ?>
+        <li><?= $post['title']?></li>
+    <?php endforeach ?>
+</ul>
 <p>
     <a href="/new_post.php">Ecrire un nouvel article</a>
 </p>
