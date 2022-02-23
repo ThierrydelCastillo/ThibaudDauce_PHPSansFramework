9ae399b732b3b184a162d296fbf8f5626af6f991
ThierrydelCastillo <dev@del-castillo.fr>
2022-02-23 14:26:05 +0100

    [PSF 06] Insertion en base de données

--- public/new_post.php
+++ public/new_post.php
@@ -1,5 +1,12 @@
 <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $path = __DIR__ . '/../database/database.sqlite';
+        $pdo = new PDO("sqlite:$path");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $pdo->prepare('INSERT INTO posts (title, body) VALUES (?, ?)')
+            ->execute([$_POST['title'], $_POST['body']]);
+            
         var_dump($_POST);
         die;
     }
