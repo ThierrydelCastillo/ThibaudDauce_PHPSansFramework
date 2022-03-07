18b9dc57e0c172c98513fc724efde88b37b238af
ThierrydelCastillo <dev@del-castillo.fr>
2022-03-07 16:33:56 +0100

    [PSF 12] Affichage d'une page 404

---
+++ html_partials/404.php
@@ -0,0 +1,3 @@
+<?php html_partial('header') ?>
+<h1>Page non trouvée.</h1>
+<?php html_partial('footer') ?>
--- public/post.php
+++ public/post.php
@@ -1,13 +1,26 @@
 <?php
     include('../bootstrap.php');
 
+    $id = filter_var($_GET['id'] ?? null, FILTER_VALIDATE_INT);
+    if($id === false) {
+        http_response_code(404);
+        html_partial('404');
+        die();
+    }
+
     $pdo = pdo();
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
     $query = $pdo->prepare('SELECT * FROM posts WHERE id = ?');
-    $query->execute([$_GET['id']]);
+    $query->execute([$id]);
 
     $post = $query->fetch();
+
+    if($post === false) {
+        http_response_code(404);
+        html_partial('404');
+        die();
+    }
 ?>
 
 <?php html_partial('header') ?>
